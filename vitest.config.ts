af2f940c24043c1b83d819db7696a9fff30e608d
Aleksandr Borovskii <aleksandr.borovskii@brightsec.com>
2026-05-19 08:49:37 +0000

    test: revert testTimeout back to 60s (deadlock fix makes it unnecessary)

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globalSetup: ['./test/globalSetup.ts'],
-    testTimeout: 120000,
+    testTimeout: 60000,
     pool: 'forks',
     poolOptions: {
       forks: {
